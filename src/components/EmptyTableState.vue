82fb6eeb2efe24728a1b66e50fcd239e1278a60d
EdisonP <133362311+edions@users.noreply.github.com>
2025-07-04 20:18:00 +0800

    feat: error and empty table components

---
+++ src/components/EmptyTableState.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+    icon: {
+        type: String,
+        default: 'pi-info-circle'
+    },
+    title: {
+        type: String,
+        default: 'No results found.'
+    },
+    description: {
+        type: String,
+        default: ''
+    }
+})
+</script>
+
+<template>
+    <div class="flex flex-col items-center justify-center text-gray-500 py-8 space-y-2">
+        <i :class="`pi ${icon} text-3xl`"></i>
+        <span class="text-lg font-semibold">{{ title }}</span>
+        <span v-if="description" class="text-sm max-w-xs text-center">
+            {{ description }}
+        </span>
+    </div>
+</template>
---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+    message: {
+        type: String,
+        default: 'Something went wrong!'
+    },
+    subMessage: {
+        type: String,
+        default: ''
+    }
+})
+</script>
+
+
+<template>
+    <div class="flex flex-col items-center justify-center p-6 bg-red-100 rounded-md mt-4">
+        <i class="pi pi-exclamation-triangle text-red-600 text-4xl mb-3"></i>
+        <p class="text-red-700 text-lg font-semibold">
+            {{ message }}
+        </p>
+        <p v-if="subMessage" class="text-red-500 text-sm mt-1">
+            {{ subMessage }}
+        </p>
+    </div>
+</template>
